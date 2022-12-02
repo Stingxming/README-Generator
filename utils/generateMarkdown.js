7eaa555c86b7ac96e3c6315bcdd88581d3eda574
Mathew Chandler <Cascabelrock@gmail.com>
2022-12-01 20:33:33 -0800

    created function for license badge and link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,31 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://www.apache.org/licenses/LICENSE-2.0)`;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink() {
+  if (license == "MIT") {
+    licenseLink = "https://opensource.org/licenses/MIT";
+  }
+  if (license == "GPLv3") {
+    licenseLink = `https://www.gnu.org/licenses/gpl-3.0`;
+  }
+  if (license == "ISC") {
+    licenseLink = `https://opensource.org/licenses/ISC`;
+  }
+  if (license == "GPL_v2") {
+    licenseLink = `https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html`;
+  }
+  if (license == "Apache_2.0") {
+    licenseLink = `https://opensource.org/licenses/Apache-2.0`;
+  }
+  if (!license) {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
